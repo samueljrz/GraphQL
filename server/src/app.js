2052b9059b688e7e8db00fd4a29e0b0968a4ea34
samueljrz <samuelevangelista.ti@gmail.com>
2021-04-14 17:51:50 -0300

    graphql ok

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,25 @@
 import express from 'express';
-import graphqlHTTP from 'express-graphql';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const graphqlHTTP = require('express-graphql').graphqlHTTP;;
+import schema from '../schema/schema.js';
+const mongoose = require('mongoose')
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
+mongoose.connect('mongodb+srv://graphqladmin:graphqlpass@cluster0.9a6rv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useFindAndModify: true,
+  useUnifiedTopology: true,
+})
 
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+})
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
 }));
 
 app.listen(3333, () => {
